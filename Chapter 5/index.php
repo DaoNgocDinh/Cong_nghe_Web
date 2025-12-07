30134d7031ec0abee2c230c3a4bc43182c1af05b
CauBeTuKy <dinhdaongoc18@gmail.com>
2025-12-07 16:34:39 +0700

    Add files via upload

---
+++ Chapter 5/index.php
@@ -0,0 +1,29 @@
+<?php
+// Tệp Controller là "não" của ứng dụng
+require_once 'models/SinhVienModel.php';
+// === THIẾT LẬP KẾT NỐI PDO ===
+// TODO 7: Copy code PDO từ PHT Chương 4 vào đây
+$host = '127.0.0.1';
+$dbname = 'cse485_web';
+$username = 'root';
+$password = '';
+$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);} 
+catch (PDOException $e) {
+    die("Kết nối thất bại: " . $e->getMessage());
+}
+// === KẾT THÚC KẾT NỐI PDO ===
+// === LOGIC CỦA CONTROLLER ===
+if (isset($_POST['ten_sinh_vien']) && isset($_POST['email'])) {
+    $ten = $_POST['ten_sinh_vien'];
+    $email = $_POST['email'];
+
+    $ok = addSinhVien($pdo, $ten, $email);
+}
+$danh_sach_sv = getAllSinhVien($pdo);
+
+include 'views/sinhvien_view.php';
+?>
